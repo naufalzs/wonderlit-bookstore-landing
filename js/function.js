0f008996722520a040951585b8b20e93e5e78d56
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-08-22 11:25:48 +0700

    add monthly top books data

--- js/function.js
+++ js/function.js
@@ -2,4 +2,9 @@ $(document).ready(function () {
   $("#toggle-menu").change(function (e) {
     $("#nav-mobile-wrapper").toggleClass("nav-wrapper--active");
   });
+  $.getJSON("/json/monthsTopBooks.json",
+    function (data) {
+      const topBooksData = data.data
+    }
+  );
 });
