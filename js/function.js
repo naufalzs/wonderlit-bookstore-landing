72e0c206bc8e62a14ebe9c28e305d1a0cb84a30a
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-08-24 17:11:33 +0700

    pair monthly top books into json data

--- js/function.js
+++ js/function.js
@@ -2,9 +2,45 @@ $(document).ready(function () {
   $("#toggle-menu").change(function (e) {
     $("#nav-mobile-wrapper").toggleClass("nav__wrapper--active");
   });
-  $.getJSON("/json/monthsTopBooks.json",
-    function (data) {
-      const topBooksData = data.data
+  
+  $.getJSON("/json/monthsTopBooks.json", function (data) {
+    function createBookEl(book) {
+      const $book = $("<div/>", {
+        class: "top-books__book",
+      });
+      const $img = $("<img>", {
+        src: book.cover,
+        alt: book.title + " book cover",
+        class: "book__cover",
+      });
+      const $title = $("<h3>", {
+        class: "book__title",
+        text: book.title,
+      });
+      const $author = $("<p>", {
+        class: "book__author",
+        text: book.author,
+      });
+      const $price = $("<div>", {
+        class: "book__price",
+        text: "$" + book.price,
+      });
+      const $btn = $("<a/>", {
+        href: "#",
+      }).prepend(
+        $("<button>", {
+          class: "btn btn--sm",
+          text: "buy now",
+        })
+      );
+      $book.append($img, $title, $author, $price, $btn);
+      return $book;
     }
-  );
+
+    const topBooksData = data.data;
+    let listBooks = topBooksData.map((item) => {
+      return createBookEl(item);
+    });
+    $(".top-books__main-content").prepend(listBooks);
+  });
 });
